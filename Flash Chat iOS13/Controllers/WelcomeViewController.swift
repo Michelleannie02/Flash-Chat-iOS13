5782dfd60f121f7201f16e8fe1a3a24e3e5fcd95
Omar Al-Dahrawy <omar_dahrawy2000@yahoo.com>
2020-08-09 01:23:35 +0200

    Added project files.

---
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+import CLTypingLabel
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = K.appName
+    }
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+       
+    }
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+        
+    }
+    
+}
